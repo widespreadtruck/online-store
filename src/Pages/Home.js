c8309ad9586ac67e54a972743bb8aba57782c39a
Nikita Guliaev <gulnikita63@gmail.com>
2020-07-23 18:26:33 -0400

    added Router

---
+++ src/Pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import {
+    PizzaBlock,
+    Categories,
+    SortPopUp
+} from '../components/index.js';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="content__top">
+
+                    <Categories items={[
+                        "Meat",
+                        "Vegan",
+                        "Grill",
+                        "Spicy",
+                        "Pies"]}
+                    />
+
+                    <SortPopUp items={[
+                        "popularity",
+                        "price",
+                        "alphabet"]}
+                    />
+
+                </div>
+                <h2 className="content__title">All pizzas</h2>
+                <div className="content__items">
+                    <PizzaBlock />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 export { default as PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
 export { default as Categories } from '../components/Categories/Categories';
 export { default as SortPopUp } from '../components/SortPopUp/SortPopUp';
+export { default as Home } from '../Pages/Home';
+export { default as Header } from '../components/Header/Header';
+export { default as Cart } from '../Pages/Cart';
 
