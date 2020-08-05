68f5f75c0d72a53067543e51c1244091a7f1ef89
Nikita Guliaev <gulnikita63@gmail.com>
2020-08-05 16:14:39 -0400

    Updates

---
+++ precache-manifest.3410af77322c044878f474c256af9c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58381e70e43b2b3a1d7779cda1fb338",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "6002287e7d8671234aa2",
+    "url": "/online-store/static/css/main.ef2e23ce.chunk.css"
+  },
+  {
+    "revision": "43b5e1814abfe01b64b3",
+    "url": "/online-store/static/js/2.f3d1c6d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/online-store/static/js/2.f3d1c6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6002287e7d8671234aa2",
+    "url": "/online-store/static/js/main.f8bdca8d.chunk.js"
+  },
+  {
+    "revision": "22496ea918f96246b60e",
+    "url": "/online-store/static/js/runtime-main.d46b2840.js"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/online-store/static/media/pizza-logo.f3327011.svg"
+  }
+]);
